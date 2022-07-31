2546ae84fbd89a3074dfe84566f3c264eb707cbf
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-07-31 12:45:54 -0300

    fetch animais refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -5,20 +5,18 @@ export default function initFetchAnimais() {
     const div = document.createElement("div");
     div.classList.add("numero-animal");
     div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
-
     return div;
   }
+
   async function fetchAnimais(url) {
     try {
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
       const numerosGrid = document.querySelector(".numeros-grid");
-
       animaisJSON.forEach((animal) => {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-
       const animaNumeros = new AnimaNumeros(
         "[data-numero]",
         ".numeros",
@@ -29,5 +27,6 @@ export default function initFetchAnimais() {
       console.log(erro);
     }
   }
+
   fetchAnimais("./animaisapi.json");
 }
